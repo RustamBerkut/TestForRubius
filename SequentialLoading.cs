d70bf7249ccffe73a28c88487bbf0f19306b19cf
RustamBerkut <achmetshinrust@gmail.com>
2022-08-01 11:58:23 +0700

    Подключаемся с сайту
    Получаем изображение с сайта и назначаем его.

--- SequentialLoading.cs
+++ SequentialLoading.cs
@@ -5,31 +5,33 @@ using UnityEngine.Networking;
 
 public class SequentialLoading : MonoBehaviour
 {
+    public RawImage rawImage;
+    private Texture2D _texture2D;
     private void Start()
     {
-        StartCoroutine(OnDownloadImage("https://picsum.photos/"));
+        StartCoroutine(OnDownloadImage("https://picsum.photos/200/300"));
     }
 
     IEnumerator OnDownloadImage(string uri)
     {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
+            webRequest.downloadHandler = new DownloadHandlerTexture();
             yield return webRequest.SendWebRequest();
-
-            string[] pages = uri.Split('/');
-            int page = pages.Length - 1;
 
             switch (webRequest.result)
             {
                 case UnityWebRequest.Result.ConnectionError:
                 case UnityWebRequest.Result.DataProcessingError:
-                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    Debug.LogError(": Error: " + webRequest.error);
                     break;
                 case UnityWebRequest.Result.ProtocolError:
-                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    Debug.LogError(": HTTP Error: " + webRequest.error);
                     break;
                 case UnityWebRequest.Result.Success:
-                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+                    _texture2D = DownloadHandlerTexture.GetContent(webRequest);
+                    rawImage.texture = _texture2D;
                     break;
             }
         }
